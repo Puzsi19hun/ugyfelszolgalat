9cbec160e3a4433ff9f9c2d94f8050cce27ab9d1
Puzsoma Sándor Kálmán <0tac-puzsan@MORICZREF.LAN>
2024-12-13 12:02:05 +0100

    das

--- ugyfelszolgalat/src/app/app.routes.ts
+++ ugyfelszolgalat/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { KapcsolatComponent } from './kapcsolat/kapcsolat.component';
 import { HibabejelentoComponent } from './hibabejelento/hibabejelento.component';
 import { DiagramComponent } from './diagram/diagram.component';
 import { HibalistaComponent } from './hibalista/hibalista.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     
-    {path: '', component: MainComponent},
+    {path: '', component: AppComponent},
     {path: 'kapcsolat', component: KapcsolatComponent},
     {path: 'diagram', component: DiagramComponent},
     {path: 'hibabejelento', component: HibabejelentoComponent},
