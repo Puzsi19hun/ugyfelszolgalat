3bf75e741f440f0be94b929323c3ce38df0c8aa3
Meszarosistvan0828 <meszarosistvan0828@gmail.com>
2024-12-13 11:47:22 +0100

    fasz234
    fasz234

--- ugyfelszolgalat/src/app/app.component.ts
+++ ugyfelszolgalat/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule],
+  imports: [RouterOutlet,HttpClientModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
