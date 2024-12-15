990506a5c98ee9bb5625229c402aa0379d451585
Puzsi19hun <125877529+Puzsi19hun@users.noreply.github.com>
2024-12-15 22:38:09 +0100

    kapcsolat

--- ugyfelszolgalat/src/app/kapcsolat/kapcsolat.component.ts
+++ ugyfelszolgalat/src/app/kapcsolat/kapcsolat.component.ts
@@ -1,13 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-kapcsolat',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule,FormsModule],
   templateUrl: './kapcsolat.component.html',
   styleUrl: './kapcsolat.component.css'
 })
 export class KapcsolatComponent {
+  
+  nev!: string;
+  email!: string;
+  uzenet!: string;
 
+
+  
+  constructor(private http: HttpClient) { }
+  
+  
+  HTTPRequest(url: string, callback: (responseText: string) => void): void {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4 && xhttp.status === 200) {
+        if (callback !== null) {
+          callback(xhttp.responseText);
+        }
+      }
+    };
+    xhttp.open('get', url, true);
+    xhttp.send();
+  }
+  
+  handleResponse(responseText: string): void {
+    console.log('Response received:', responseText);
+  }
+
+  sendEmail(form: NgForm) {
+    this.HTTPRequest('https://puzsisanyi.moriczcloud.hu/api/ugyfelszolgalat/email/' + this.nev + '/' + this.email + '/' + this.uzenet, this.handleResponse);
+    form.resetForm()
+  }
 }
